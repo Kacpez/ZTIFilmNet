74f6178951c6634a197f38a8e4b8da7a686bb85d
Kacpez <pokacper.goraj@interia.pl>
2023-06-21 11:22:53 +0200

    cross origin

--- src/main/java/zti/FilmNet/controller/StreamingPlatformController.java
+++ src/main/java/zti/FilmNet/controller/StreamingPlatformController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://ztifilmnet-frontend-production.up.railway.app")
 @RestController
 @RequestMapping("/api/v1/streaming_platforms")
 public class StreamingPlatformController {
